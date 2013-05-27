8a9639d6eeb0bf1f2c40f9d3272658ac55142fbb
JLarky <jlarky@gmail.com>
2013-05-27 11:11:07 +0400

    scripts to test lechat server

---
+++ lib/services/lechat_webhook.rb
@@ -0,0 +1,28 @@
+class Service::LeChatWebhook < Service
+  string :webhook_url
+
+  # include 'webhook_url' in the debug logs
+  white_list :webhook_url
+
+  default_events :push
+
+  url "https://lechat.im/"
+
+  maintained_by :github => 'JLarky'
+
+  supported_by :email => 'support@lechat.im'
+
+  def receive_event
+    if data['webhook_url'].to_s.empty?
+      raise_config_error "webhook_url url is missing"
+    end
+
+    http.headers['content-type'] = 'application/x-www-form-urlencoded'
+    res = http_post data['webhook_url'],
+      "payload" => generate_json(payload)
+
+    if res.status < 200 || res.status > 299
+      raise_config_error("Unexpected response code:#{res.status}")
+    end
+  end
+end
